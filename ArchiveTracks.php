9f985a4ffc85625c0194fd2ee0e436e34022da2e
Dan Corneanu <dan.corneanu@youdo.co.nz>
2017-09-24 22:38:21 +1300

    Fix require path so that cron jobs won't fail anymore.

--- ArchiveTracks.php
+++ ArchiveTracks.php
@@ -1,6 +1,6 @@
 <?php
-$db_params = require('./config/database.php');
-$con1_params = $db_params['gliding']; 
+$db_params = require dirname(__FILE__).'/config/database.php';
+$con1_params = $db_params['gliding'];
 $con1=mysqli_connect($con1_params['hostname'],$con1_params['username'],$con1_params['password'],$con1_params['dbname']);
 if (mysqli_connect_errno())
 {
@@ -24,14 +24,14 @@ $dtPrev->setTimestamp($dtNow->getTimestamp() - (3600*24*3));
 
 
 $q = "SELECT org,glider,point_time,point_time_milli,lattitude,longitude,altitude,accuracy from tracks where point_time < '" . $dtPrev->format('Y-m-d') . "'";
-$r = mysqli_query($con1,$q);  
+$r = mysqli_query($con1,$q);
 while ($row = mysqli_fetch_array($r))
 {
    $q1 = "INSERT INTO tracksarchive (org,glider,point_time,point_time_milli,lattitude,longitude,altitude,accuracy) VALUES (".$row[0].",'".$row[1]."','".$row[2]."',".$row[3].",".$row[4].",".$row[5].",".$row[6].",".$row[7].")";
-   $r1 = mysqli_query($con2,$q1);  
+   $r1 = mysqli_query($con2,$q1);
 }
 $q = "DELETE from tracks where point_time < '" . $dtPrev->format('Y-m-d') . "'";
-$r = mysqli_query($con1,$q); 
+$r = mysqli_query($con1,$q);
 mysqli_close($con1);
 mysqli_close($con2);
 ?>
--- TracksRemoveRedundant.php
+++ TracksRemoveRedundant.php
@@ -6,14 +6,15 @@ include 'timehelpers.php';
 function HaveFlight($db,$dt,$glider)
 {
    $bt = $dt->getTimestamp();
-   $q1 = "Select id from flights where flights.glider = '".$glider."' and (flights.start/1000) <= ".$bt." and (flights.land/1000) >= " .$bt; 
-   $r1 = mysqli_query($db,$q1); 
+   $q1 = "Select id from flights where flights.glider = '".$glider."' and (flights.start/1000) <= ".$bt." and (flights.land/1000) >= " .$bt;
+   $r1 = mysqli_query($db,$q1);
    if (mysqli_num_rows($r1)  > 0)
      return true;
    return false;
 }
 
-$con_params = require('./config/database.php'); $con_params = $con_params['gliding']; 
+$con_params = require dirname(__FILE__).'/config/database.php';
+$con_params = $con_params['gliding'];
 $con=mysqli_connect($con_params['hostname'],$con_params['username'],$con_params['password'],$con_params['dbname']);
 if (mysqli_connect_errno())
 {
@@ -25,13 +26,13 @@ $cntNoFlight = 0;
 $cnt = 0;
 
 $q = "SELECT id, point_time , glider from tracks";
-$r = mysqli_query($con,$q);  
+$r = mysqli_query($con,$q);
 while ($row = mysqli_fetch_array($r))
 {
   $cnt = $cnt + 1;
   $id = $row[0];
   $pdt = new DateTime($row[1]);
-  
+
   if (HaveFlight($con,$pdt,$row[2]) )
   {
      $cntFlight = $cntFlight + 1;
@@ -40,7 +41,7 @@ while ($row = mysqli_fetch_array($r))
   {
      $cntNoFlight = $cntNoFlight + 1;
      $q2 =  "DELETE from tracks where id = " . $id;
-     $r2 = mysqli_query($con,$q2); 
+     $r2 = mysqli_query($con,$q2);
   }
 }
 echo "<p>Tracks with flights = " .$cntFlight. "</p>";
