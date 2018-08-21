08eee03ab3b42d470bd3905e325cc768fbda7de9
Dan Corneanu <dan.corneanu@youdo.co.nz>
2018-08-21 22:05:51 +1200

    Group A/B Cat Instructor and C Cat Instructor into Instructor. Only display a selection of the roles in the report.

--- lrv/app/Services/Reports/MembersRolesStats.php
+++ lrv/app/Services/Reports/MembersRolesStats.php
@@ -7,18 +7,29 @@ use App\Models\Flight;
 use App\Models\Member;
 use App\Models\Role;
 use App\Models\MembershipStatus;
+use Illuminate\Support\Collection;
 use DB;
+use stdClass;
 
 class MembersRolesStats
 {
   public static function build($user, $org) {
-    $roleNames = Role::all()->pluck('name', 'id')->all();
+    $roleNames = [
+      Role::ROLE_AB_CAT_INSTRUCTOR,
+      Role::ROLE_C_CAT_INSTRUCTOR,
+      Role::ROLE_WINCH_DRIVER,
+      Role::ROLE_LPC,
+      Role::ROLE_ENGINEER,
+      Role::ROLE_TOW_PILOT,
+      Role::ROLE_CMT
+    ];
 
     $roleCountSql = DB::table('members')
       ->select('members.id AS memberId', DB::raw('count(member_roles.id) as roleCount'))
       ->leftJoin('role_member AS member_roles', 'member_roles.member_id', '=', 'members.id')
+      ->leftJoin('roles AS roles', 'roles.id', '=', 'member_roles.role_id')
+      ->whereIn('roles.name', $roleNames)
       ->where('members.org', $org)
-
       ->groupBy('members.id');
 
     $members = DB::table(DB::raw('(' . $roleCountSql->toSql() . ')  as roleCounts'))
@@ -32,9 +43,37 @@ class MembersRolesStats
       ->orderBy('roleCounts.roleCount', 'desc')
       ->orderBy('members.id');
 
+    $byMemberId = $members->get()->groupBy(function($item){
+      return $item->id;
+    });
+    $membersWithRoles = $byMemberId->map(function($rows, $id){
+      $member = new stdClass();
+      $member->id = $id;
+      $member->roles = [];
+      $rows->each(function($item) use ($member) {
+        $member->displayname = $item->displayname;
+        $member->roles[] = $item->roleName;
+      });
+
+      $member->roles = self::regroupRoles($member->roles);
+      return $member;
+    });
+
     return [
-      'roleNames' => $roleNames,
-      'members' => $members->get(),
+      'roleNames' => self::regroupRoles($roleNames),
+      'members' => $membersWithRoles,
     ];
+  }
+
+  private static function regroupRoles($roles) {
+    return (new Collection($roles))->map(function ($roleName) {
+      switch ($roleName) {
+        case Role::ROLE_AB_CAT_INSTRUCTOR:
+        case Role::ROLE_C_CAT_INSTRUCTOR:
+          return 'Instructor';
+        default:
+          return $roleName;
+      };
+    })->unique();
   }
 }
--- lrv/resources/views/reports/membersRolesStatsReport.blade.php
+++ lrv/resources/views/reports/membersRolesStatsReport.blade.php
@@ -16,38 +16,23 @@
     <thead>
       <tr>
         <th>User/Role</th>
-        @foreach(array_values($roleNames) as $roleName)
+        @foreach($roleNames as $roleName)
           <th>{{$roleName}}</th>
         @endforeach
       </tr>
     </thead>
 
-    @php
-      $currentMember = new stdClass();
-      $currentMember->id = null;
-    @endphp
     @foreach($members as $member)
-      @if($member->id != $currentMember->id)
-        @if($currentMember->id != null)
-          <tr>
-            <td>{{$member->displayname}}</td>
-            @foreach(array_values($roleNames) as $roleName)
-              @if(in_array($roleName, $currentMember->roles))
-                <td>&#x2713;</td>
-              @else
-                <td></td>
-              @endif
-            @endforeach
-          </tr>
-        @endif
-        @php
-          $currentMember = $member;
-          $currentMember->roles = [];
-        @endphp
-      @endif
-      @php
-        $currentMember->roles[] = $member->roleName;
-      @endphp
+      <tr>
+        <td>{{$member->displayname}}</td>
+        @foreach($roleNames as $roleName)
+          @if($member->roles->contains($roleName))
+            <td>&#x2713;</td>
+          @else
+            <td></td>
+          @endif
+        @endforeach
+      </tr>
     @endforeach
   </table>
 @endsection
