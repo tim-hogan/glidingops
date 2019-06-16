e0d2c17d2ae685a2bb2a4cc93b41450d5f2e4538
tim-hogan <tim@mobilelocate.co.nz>
2019-06-17 10:45:12 +1200

    Site configuration

---
+++ config/site.php
@@ -0,0 +1,12 @@
+<?php
+/*
+----------------------------------------------------
+  Site settings file for global site settings
+----------------------------------------------------
+*/
+  return [
+    'globalSettings' => [
+        'mapKey' => getenv ('MAP_API_KEY')
+    ]
+  ];
+?>
