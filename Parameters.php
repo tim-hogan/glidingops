68ad6c05211994ed2ab31d502093cc6cb27b26fc
tim-hogan <tim@mobilelocate.co.nz>
2021-02-20 15:13:01 +1300

    New Server

---
+++ Parameters.php
@@ -0,0 +1,216 @@
+<?php
+/**
+ * @abstract Template Code for the a MultiPart Form List page
+ * @author Tim Hogan
+ * @version 1.0
+ * @requires classSecure (Which includes clasEnvironment, clasVault and secruityParams ), classFormList2.php
+ * Search for **EDIT** for places to edit
+ */
+session_start();
+function var_error_log( $object=null,$text='')
+{
+    ob_start();
+    var_dump( $object );
+    $contents = ob_get_contents();
+    ob_end_clean();
+    error_log( "{$text} {$contents}" );
+}
+
+require_once "./includes/classSecure.php";
+require_once "./includes/classRolling.php";
+require "./includes/classFormList2.php";
+$formdata = require("./forms/**EDIT**.php");
+
+
+$selff = trim($_SERVER["PHP_SELF"],"/");
+$user = null;
+if (isset($_SESSION['userid']))
+    $user = $DB->getUser($_SESSION['userid']);
+Secure::CheckPage2($user,SECURITY_ADMIN);
+
+$pageData = array();
+$pageData ['select'] = 'global';
+$pageData ['form'] = array();
+$pageData ['form'] ['display'] = false;
+$pageData ['form'] ['mode'] = "";
+$pageData ['form'] ['recid'] = "";
+
+
+if (isset($_GET['v']))
+{
+    $a = FormList::decryptParamRaw($_GET['v']);
+    if (isset($a['action']))
+    {
+        switch ($a['action'])
+        {
+            case "create":
+                $table = $a['table'];
+                $pageData ['select'] = $table;
+                $pageData ['form'] ['display'] = true;
+                $pageData ['form'] ['mode'] = "create";
+                break;
+            case "edit":
+                $table = $a['table'];
+                $pageData ['select'] = $table;
+                $pageData ['form'] ['display'] = true;
+                $pageData ['form'] ['mode'] = "edit";
+                if (isset($a['onerec']))
+                    $pageData ['form'] ['recid'] = -99;
+                else
+                    $pageData ['form'] ['recid'] = $a['id'];
+                break;
+        }
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    //Check the CSRF
+    if (!Secure::checkCSRF() )
+    {
+        $DB->createAudit("security","{$selff} [" .__LINE__. "] Admin csrf failed");
+        header("Location: SecurityError.php");
+        exit();
+    }
+
+    if (isset($_POST['v']))
+    {
+        $a = FormList::decryptParamRaw($_POST['v']);
+        if (isset($a['table']) && isset($a['action']))
+        {
+            $FL = new FormList($formdata[$a['table']]);
+            $valid = $FL->getFormInputFields();
+            if ($valid && $a['action'] == 'change')
+                $FL->ModifyRecord($DB,$a['recid']);
+            if ($valid && $a['action'] == 'create')
+                $FL->AddRecord($DB);
+            $pageData ['select'] = $a['table'];
+        }
+    }
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="initial-scale=1.0" />
+    <title>**EDIT**</title>
+    <link rel='stylesheet' type='text/css' href='css/base.css' />
+    <link rel='stylesheet' type='text/css' href='css/heading.css' />
+    <link rel='stylesheet' type='text/css' href='css/menu.css' />
+    <link rel='stylesheet' type='text/css' href='css/main.css' />
+    <link rel='stylesheet' type='text/css' href='css/form.css' />
+    <link rel='stylesheet' type='text/css' href='css/list.css' />
+    <link rel='stylesheet' type='text/css' href='css/**EDIT**.css' />
+    **EDIT**<script src="/js/MultiForm.js"></script>
+</head>
+<body onload="start()">
+    <div id="container">
+        <div id="heading">
+            <h1>**EDIT**HEADING</h1>
+        </div>
+        <div id="menu">
+            <p>**EDIT**MENU</p>
+        </div>
+        <div id="main">
+            <div id="flexcontainer">
+                <div id="left">
+                    <div class="minimiser" expanded="1" minsize="20" onclick="minmaxwinddow(this)" title="Minimise"><<</div>
+                    <div class="panel">
+                        <p class="lefttitle">**EDIT**</p>
+                        <ul>
+                            <li id="selglobal" class="liselector" onclick="selectRight(this,'global')">**EDIT**Global Parameters</li>
+                            <li id="selserver" class="liselector" onclick="selectRight(this,'server')">**EDIT**Servers</li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="right">
+                    <div class="minimiser" expanded="1" minsize="20" onclick="minmaxwinddow(this)"><<</div>
+                    <div class="panel">
+                        <div id="global" class="rtEntity">
+                            <div id="listglobals">
+                                <?php
+                                $FL = new FormList($formdata['global']);
+                                $FL->buildList($DB,null);
+                                ?>
+                            </div>
+                        </div>
+                        <div id="server" class="rtEntity">
+                            <div id="listservers">
+                                <?php
+                                $FL = new FormList($formdata['server']);
+                                $FL->buildList($DB,null);
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="rightdetail">
+                    <div class="hider" expanded="1" minsize="20" onclick="hidewinddow(this)">X</div>
+                    <div class="panel">
+                        <div id="globalform" class="detailEntity">
+                            <div class="form">
+                            <form method="POST" autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                                <?php
+                                if ($pageData ['select'] == 'global')
+                                {
+                                    $FL = new FormList($formdata['global']);
+                                    if ($pageData ['form'] ['mode'] == "edit")
+                                        $FL->getTableData($DB,$pageData ['form'] ['recid']);
+                                    $FL->buildFormFields(null,$DB);
+                                    echo "<div class='submit'>";
+                                    if ($pageData ['form'] ['mode'] == "edit")
+                                    {
+                                        $v = FormList::encryptParam("table=globalr&action=change&recid={$pageData ['form'] ['recid']}");
+                                        echo "<input type='hidden' name='v' value='{$v}' />";
+                                        echo "<input type='submit' name='_server_change' value='CONFIRM CHANGE' />";
+                                    }
+                                    else
+                                    {
+                                        $v = FormList::encryptParam("table=global&action=create");
+                                        echo "<input type='hidden' name='v' value='{$v}' />";
+                                        echo "<input type='submit' name='_server_new' value='CREATE NEW' />";
+                                    }
+                                    echo "</div>";
+                                }
+                                ?>
+                            </form>
+                            </div>
+                        </div>
+                        <div id="serverform" class="detailEntity">
+                            <div class="form">
+                            <form method="POST" autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                                <?php
+                                if ($pageData ['select'] == 'server')
+                                {
+                                    $FL = new FormList($formdata['server']);
+                                    if ($pageData ['form'] ['mode'] == "edit")
+                                        $FL->getTableData($DB,$pageData ['form'] ['recid']);
+                                    $FL->buildFormFields(null,$DB);
+                                    echo "<div class='submit'>";
+                                    if ($pageData ['form'] ['mode'] == "edit")
+                                    {
+                                        $v = FormList::encryptParam("table=server&action=change&recid={$pageData ['form'] ['recid']}");
+                                        echo "<input type='hidden' name='v' value='{$v}' />";
+                                        echo "<input type='submit' name='_server_change' value='CONFIRM CHANGE' />";
+                                    }
+                                    else
+                                    {
+                                        $v = FormList::encryptParam("table=server&action=create");
+                                        echo "<input type='hidden' name='v' value='{$v}' />";
+                                        echo "<input type='submit' name='_server_new' value='CREATE NEW' />";
+                                    }
+                                    echo "</div>";
+                                }
+                                ?>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ js/MultiForm.js
@@ -0,0 +1,97 @@
+// JavaScript source code
+//Generic formlist JS
+function deleteButtonChange(tbl) {
+    var but = document.getElementById("del" + tbl);
+    var l = document.getElementsByClassName('listcheck' + tbl);
+    but.disabled = true;
+    for (var i = 0; i < l.length; i++) {
+        if (l[i].checked) {
+            but.disabled = false;
+        }
+    }
+}
+//This page
+var g_pageState = JSON.parse('<?php echo json_encode($pageAdminHome);?>');
+function minmaxwinddow(n) {
+    //Get the parenet div
+    var e = parseInt(n.getAttribute("expanded"));
+    var m = parseInt(n.getAttribute("minsize"));
+    var p = n.parentElement;
+    if (e == 1) {
+        p.fullwidth = p.style.width;
+        p.style.width = m + "px";
+        n.innerHTML = ">>";
+        n.setAttribute("expanded", "0");
+        n.setAttribute("title", "Maximise Panel");
+    }
+    else {
+        p.style.width = p.fullwidth;
+        n.innerHTML = "<<";
+        n.setAttribute("expanded", "1");
+        n.setAttribute("title", "Minimise Panel");
+    }
+}
+
+function hideAllRightForms() {
+    document.getElementById('rightdetail').style.display = 'none';
+    var l = document.getElementsByClassName('detailEntity');
+    for (var i = 0; i < l.length; i++) {
+        l[i].style.display = "none";
+    }
+}
+
+
+function hidewinddow(n) {
+    var p = n.parentElement;
+    p.style.display = 'none';
+
+    var l = document.getElementsByClassName('detailEntity');
+    for (var i = 0; i < l.length; i++) {
+        l[i].style.display = "none";
+    }
+}
+
+function selectRight(n, name) {
+    hideAllRightForms();
+    g_pageState.select = name;
+    console.log("selectRight2 g_PageState = " + JSON.stringify(g_pageState));
+
+
+    var l = document.getElementsByClassName('rtEntity');
+    for (var i = 0; i < l.length; i++) {
+        if (l[i].id == name) {
+            console.log(" Display " + name);
+            l[i].style.display = "block";
+        }
+        else {
+            l[i].style.display = "none";
+        }
+    }
+    var l = document.getElementsByClassName('liselector');
+    for (var i = 0; i < l.length; i++) {
+        if (l[i] == n) {
+            l[i].style.color = "#b92e02";
+            l[i].style.fontWeight = "bold";
+            l[i].style.paddingLeft = "0px";
+            l[i].style.paddingTop = "10px";
+            l[i].style.paddingBottom = "10px";
+            l[i].style.fontSize = "13pt";
+        }
+        else {
+            l[i].style.color = "";
+            l[i].style.fontWeight = "";
+            l[i].style.paddingLeft = "";
+            l[i].style.paddingTop = "";
+            l[i].style.paddingBottom = "";
+            l[i].style.fontSize = "";
+        }
+    }
+}
+function start() {
+    var n = document.getElementById('sel' + g_pageState.select);
+    selectRight(n, g_pageState.select);
+    if (g_pageState.form.display) {
+        document.getElementById('rightdetail').style.display = 'block';
+        document.getElementById(g_pageState.select + 'form').style.display = 'block';
+    }
+}
