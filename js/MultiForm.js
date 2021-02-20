3c64e3c33c5188be552fa7144d3e251d76df230a
tim-hogan <tim@mobilelocate.co.nz>
2021-02-20 17:58:09 +1300

    Update MultiForm.js

--- js/MultiForm.js
+++ js/MultiForm.js
@@ -11,7 +11,7 @@ function deleteButtonChange(tbl) {
     }
 }
 //This page
-var g_pageState = JSON.parse('<?php echo json_encode($pageAdminHome);?>');
+var g_pageState = JSON.parse('<?php echo json_encode($pageData);?>');
 function minmaxwinddow(n) {
     //Get the parenet div
     var e = parseInt(n.getAttribute("expanded"));
