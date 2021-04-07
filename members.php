f35f2821e5f8eb157be48ab657900284a70483a6
brunotag <bruno.tagliapietra@gmail.com>
2021-04-07 21:03:02 +1200

    fix: allow removing all the roles from a member

--- members.php
+++ members.php
@@ -472,11 +472,14 @@ if ($_SESSION['security'] & 16) {       $Q.= ",";
         $recid = $con->insert_id;
       }
 
-      if(!isset($_POST["del"]) && isset($_POST["roles"])) {
+      if(!isset($_POST["del"])) {
+        $member =  App\Models\Member::find($recid);
         // Update Roles collection from roles[] POST parameter
         if (is_array($roleIds)) {
-          $member =  App\Models\Member::find($recid);
           $member->setRoles($org, $roleIds);
+        }
+        else {
+          $member->setRoles($org, []);
         }
       }
 
