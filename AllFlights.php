30c1286dae716ea13da01f400f6fa45c91b67182
tim-hogan <tim@mobilelocate.co.nz>
2021-04-30 13:42:37 +1200

    Update AllFlights.php

--- AllFlights.php
+++ AllFlights.php
@@ -129,6 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
                                 $strLaunch = (new DateTime())->setTimestamp($flight['start'] / 1000)->setTimezone(new DateTimeZone($organistaion['timezone']))->format('H:i');
                                 $strLand = (new DateTime())->setTimestamp($flight['land'] / 1000)->setTimezone(new DateTimeZone($organistaion['timezone']))->format('H:i');
                                 $duration = ($flight['land'] / 1000) - ($flight['start'] / 1000) / 60;
+                                error_log("Duration {$duration}");
                                 $strDuration = sprintf("%02d",$duration / 60) . ":" . sprintf("%02d",$duration % 60);
 
                                 echo "<tr>";
